2f565afe1ec886569a170b8e5aaab6d1ee004d71
jashgopani <jash.gopani@somaiya.edu>
2021-05-03 23:40:30 +0530

    added package.json and function

---
+++ nearby.js
@@ -0,0 +1,26 @@
+module.exports = function nearbyElements(directions = 8) {
+    const angles = Array(directions)
+        .fill(0.25)
+        .map((val, i) => {
+            return Math.PI * i * val;
+        });
+
+    return (e, predicate) => {
+        const x = e.clientX; //x position within the element.
+        const y = e.clientY; //y position within the element.
+        return angles.reduce((acc, rad) => {
+            const cx = Math.floor(x + Math.cos(rad) * offset);
+            const cy = Math.floor(y + Math.sin(rad) * offset);
+            const element = document.elementFromPoint(cx, cy);
+            if (
+                element &&
+                acc.findIndex((ae) => ae.id === element.id) < 0 &&
+                predicate(element)
+            ) {
+                return [...acc, element];
+            }
+
+            return acc;
+        });
+    };
+};
